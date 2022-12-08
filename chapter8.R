f02ec68db749225f2932a07c3dcf8d09ec94b017
Pierre <pierre.klintefors@gmail.com>
2022-12-08 11:39:23 +0100

    started on h3

--- chapter8.R
+++ chapter8.R
@@ -410,5 +410,36 @@ plot(compare(H1, H2, WAIC = T))
 # does not give reliable more weight to the model that includes bed. 
 
 #H3
+#R code 8.1
+library(rethinking)
+data(rugged)
+d <- rugged
+
+# make log version of outcome
+d$log_gdp <- log( d$rgdppc_2000 )
+
+# extract countries with GDP data
+dd <- d[ complete.cases(d$rgdppc_2000) , ]
+
+# rescale variables
+dd$log_gdp_std <- dd$log_gdp / mean(dd$log_gdp)
+dd$rugged_std <- dd$rugged / max(dd$rugged)
 
 
+
+#Including information about African and non-african continent in the model
+#R code 8.7
+# make variable to index Africa (1) or not (2)
+dd$cid <- ifelse( dd$cont_africa==1 , 1 , 2 )
+
+#R code 8.8
+m8.2 <- quap(
+  alist(
+    log_gdp_std ~ dnorm( mu , sigma ) ,
+    mu <- a[cid] + b*( rugged_std - 0.215 ) ,
+    a[cid] ~ dnorm( 1 , 0.1 ) ,
+    b ~ dnorm( 0 , 0.3 ) ,
+    sigma ~ dexp( 1 )
+  ) , data=dd )
+
+max(WAIC(m8.2, pointwise = T)$WAIC)
