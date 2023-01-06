d07c3b2ed2f0beceadc6a73bfb87a9c27bed3bcd
Pierre <pierre.klintefors@gmail.com>
2023-01-06 09:49:55 +0100

    Multinomial models

--- chapter11.R
+++ chapter11.R
@@ -191,3 +191,463 @@ stancode(m11.4)
 m11.4_stan_code <- stancode(m11.4)
 m11.4_stan <- stan( model_code=m11.4_stan_code , data=dat_list , chains=4 )
 compare( m11.4_stan , m11.4 )
+
+#Proportional odds of switching from treatment 2 to 4
+#R code 11.23
+post <- extract.samples(m11.4)
+mean( exp(post$b[,4]-post$b[,2]) )
+
+
+#Calculating number of times left lever were pulled on each
+#conditional predictor
+#R code 11.24
+data(chimpanzees)
+d <- chimpanzees
+d$treatment <- 1 + d$prosoc_left + 2*d$condition
+d$side <- d$prosoc_left + 1 # right 1, left 2
+d$cond <- d$condition + 1 # no partner 1, partner 2
+d_aggregated <- aggregate(d$pulled_left , 
+                          list( treatment=d$treatment , 
+                                actor=d$actor ,
+                                side=d$side , 
+                                cond=d$cond ), sum )
+colnames(d_aggregated)[5] <- "left_pulls"
+
+d_aggregated
+
+#Model with pull frequency instead of proportion
+#R code 11.25
+dat <- with( d_aggregated , list(
+  left_pulls = left_pulls,
+  treatment = treatment,
+  actor = actor,
+  side = side,
+  cond = cond ) )
+
+m11.6 <- ulam(
+  alist(
+    left_pulls ~ dbinom( 18 , p ) ,
+    logit(p) <- a[actor] + b[treatment] ,
+    a[actor] ~ dnorm( 0 , 1.5 ) ,
+    b[treatment] ~ dnorm( 0 , 0.5 )
+  ) , data=dat , chains=4, cores = 4 , log_lik=TRUE )
+
+#R code 11.26
+compare( m11.6 , m11.4 , func=PSIS )
+
+#The aggregated model m11.6 contains an extra factor in 
+#its log-probabilities. 
+
+#R code 11.27
+# deviance of aggregated 6-in-9
+-2*dbinom(6,9,0.2,log=TRUE)
+# deviance of dis-aggregated
+-2*sum(dbern(c(1,1,1,1,1,1,0,0,0),0.2,log=TRUE))
+
+
+#Graduate school admissions, biased by geneder?
+#R code 11.28
+library(rethinking)
+data(UCBadmit)
+d <- UCBadmit
+
+
+#model that includes gender as predictior
+#R code 11.29
+dat_list <- list(
+  admit = d$admit,
+  applications = d$applications,
+  gid = ifelse( d$applicant.gender=="male" , 1 , 2 ) #index variable
+)
+m11.7 <- ulam(
+  alist(
+    admit ~ dbinom( applications , p ) ,
+    logit(p) <- a[gid] ,
+    a[gid] ~ dnorm( 0 , 1.5 )
+  ) , data=dat_list , chains=4 )
+precis( m11.7 , depth=2 )
+
+
+#The differences beween gender on different scales
+#R code 11.30
+post <- extract.samples(m11.7)
+diff_a <- post$a[,1] - post$a[,2]
+diff_p <- inv_logit(post$a[,1]) - inv_logit(post$a[,2]) #probability scale
+precis( list( diff_a=diff_a , diff_p=diff_p ) )
+
+
+#R code 11.31
+postcheck( m11.7 )
+# draw lines connecting points from same dept
+for ( i in 1:6 ) {
+  x <- 1 + 2*(i-1)
+  y1 <- d$admit[x]/d$applications[x]
+  y2 <- d$admit[x+1]/d$applications[x+1]
+  lines( c(x,x+1) , c(y1,y2) , col=rangi2 , lwd=2 )
+  text( x+0.5 , (y1+y2)/2 + 0.05 , d$dept[x] , cex=0.8 , col=rangi2 )
+}
+
+#The posterior validation checks shows that female ar only less admitted in 
+#two of the 12 departments but the overall probability is still lower for females
+#This is because the number of total applicants differs between department and
+#also the number of female applicants.
+ 
+# Instead of asking “What are the average probabilities of admission for women and
+# men across all departments?” we want to ask “What is the average difference in 
+# probability of admission between women and men within departments?
+
+
+#New model with a index variable for department so each department gets it own
+# log-odds
+
+#R code 11.32
+dat_list$dept_id <- rep(1:6,each=2)
+m11.8 <- ulam(
+  alist(
+    admit ~ dbinom( applications , p ) ,
+    logit(p) <- a[gid] + delta[dept_id] ,
+    a[gid] ~ dnorm( 0 , 1.5 ) ,
+    delta[dept_id] ~ dnorm( 0 , 1.5 )
+  ) , data=dat_list , chains=4 , iter=4000 )
+precis( m11.8 , depth=2 )
+
+#Contrasts of gender on two scales
+#R code 11.33
+post <- extract.samples(m11.8)
+diff_a <- post$a[,1] - post$a[,2]
+diff_p <- inv_logit(post$a[,1]) - inv_logit(post$a[,2])
+precis( list( diff_a=diff_a , diff_p=diff_p ) )
+
+
+#Distribtions of apllid departments
+#R code 11.34
+pg <- with( dat_list , sapply( 1:6 , function(k)
+  applications[dept_id==k]/sum(applications[dept_id==k]) ) )
+rownames(pg) <- c("male","female")
+colnames(pg) <- unique(d$dept)
+round( pg , 2 )
+
+
+###### Poisson regression #####################
+
+#When a binominal distribution have a large sample and small probability of an 
+#event, the expected value and the variance approximates each other.
+# E = Np
+# Variance = Np(1-p)
+
+#R code 11.35
+y <- rbinom(1e5,1000,1/1000)
+c( mean(y) , var(y) )
+
+#This is the Poission distribution that only have one lmambda parameter
+#that is both the expected value and the expected variance
+
+
+#Data of island population sizes and their oceanitc tool kit size
+#R code 11.36
+library(rethinking)
+data(Kline)
+d <- Kline
+d
+
+#Tool kit size is expected to be related to the log 
+#of population size because it is the order of magnitude 
+#in size increase that is of relavance
+
+#R code 11.37
+d$P <- scale( log(d$population) )
+d$contact_id <- ifelse( d$contact=="high" , 2 , 1 )
+
+
+#the transformation of the scale beween the scale of the model and the
+# count scale of the outcomes means that a flat prior is not necessary flat on the
+#outcome scale
+
+#Log normal distribution
+#R code 11.38
+curve( dlnorm( x , 6 , 2 ) , from=0 , to=100 , n=200 )
+
+#The mean becomes very big
+#R code 11.39
+a <- rnorm(1e4,0,10)
+lambda <- exp(a)
+mean( lambda )
+
+#Flat priors does not work so good in poission models
+
+#R code 11.40, weakly informative piror
+curve( dlnorm( x , 3 , 0.5 ) , from=0 , to=100 , n=200 )
+
+
+#Simulation with conventionnly flat piror
+
+#R code 11.41
+N <- 100
+a <- rnorm( N , 3 , 0.5 )
+b <- rnorm( N , 0 , 10 )
+plot( NULL , xlim=c(-2,2) , ylim=c(0,100) )
+for ( i in 1:N ) curve( exp( a[i] + b[i]*x ) , add=TRUE , col=grau() )
+
+
+#Tighter prior
+#R code 11.42
+set.seed(10)
+N <- 100
+a <- rnorm( N , 3 , 0.5 )
+b <- rnorm( N , 0 , 0.2 )
+plot( NULL , xlim=c(-2,2) , ylim=c(0,100) )
+for ( i in 1:N ) curve( exp( a[i] + b[i]*x ) , add=TRUE , col=grau() )
+
+# with unstandardized population
+#R code 11.43
+x_seq <- seq( from=log(100) , to=log(200000) , length.out=100 )
+lambda <- sapply( x_seq , function(x) exp( a + b*x ) )
+plot( NULL , xlim=range(x_seq) , ylim=c(0,500) , xlab="log population" ,
+      ylab="total tools" )
+for ( i in 1:N ) lines( x_seq , lambda[i,] , col=grau() , lwd=1.5 )
+
+
+#R code 11.44
+
+plot( NULL , xlim=range(exp(x_seq)) , ylim=c(0,500) , xlab="population" ,
+      ylab="total tools" )
+for ( i in 1:N ) lines( exp(x_seq) , lambda[i,] , col=grau() , lwd=1.5 )
+
+
+#Intercept and interaction model
+
+#R code 11.45
+dat <- list(
+  T = d$total_tools ,
+  P = d$P ,
+  cid = d$contact_id )
+
+
+# intercept only
+m11.9 <- ulam(
+  alist(
+    T ~ dpois( lambda ),
+    log(lambda) <- a,
+    a ~ dnorm( 3 , 0.5 )
+  ), data=dat , chains=4 , log_lik=TRUE )
+
+# interaction model
+m11.10 <- ulam(
+  alist(
+    T ~ dpois( lambda ),
+    log(lambda) <- a[cid] + b[cid]*P,
+    a[cid] ~ dnorm( 3 , 0.5 ),
+    b[cid] ~ dnorm( 0 , 0.2 )
+  ), data=dat , chains=4 , log_lik=TRUE )
+
+#R code 11.46
+compare( m11.9 , m11.10 , func=PSIS )
+
+#R code 11.47
+k <- PSIS( m11.10 , pointwise=TRUE )$k
+plot( dat$P , dat$T , xlab="log population (std)" , ylab="total tools" ,
+    col=rangi2 , pch=ifelse( dat$cid==1 , 1 , 16 ) , lwd=2 ,
+      ylim=c(0,75) , cex=1+normalize(k) )
+
+# set up the horizontal axis values to compute predictions at
+ns <- 100
+P_seq <- seq( from=-1.4 , to=3 , length.out=ns )
+
+# predictions for cid=1 (low contact)
+lambda <- link( m11.10 , data=data.frame( P=P_seq , cid=1 ) )
+lmu <- apply( lambda , 2 , mean )
+lci <- apply( lambda , 2 , PI )
+lines( P_seq , lmu , lty=2 , lwd=1.5 )
+shade( lci , P_seq , xpd=TRUE )
+
+# predictions for cid=2 (high contact)
+lambda <- link( m11.10 , data=data.frame( P=P_seq , cid=2 ) )
+lmu <- apply( lambda , 2 , mean )
+lci <- apply( lambda , 2 , PI )
+lines( P_seq , lmu , lty=1 , lwd=1.5 )
+shade( lci , P_seq , xpd=TRUE )
+
+
+#Plot with natrual scale
+#R code 11.48
+plot( d$population , d$total_tools , xlab="population" , ylab="total tools" ,
+      col=rangi2 , pch=ifelse( dat$cid==1 , 1 , 16 ) , lwd=2 ,
+      ylim=c(0,75) , cex=1+normalize(k) )
+ns <- 100
+P_seq <- seq( from=-5 , to=3 , length.out=ns )
+# 1.53 is sd of log(population)
+# 9 is mean of log(population)
+pop_seq <- exp( P_seq*1.53 + 9 )
+
+lambda <- link( m11.10 , data=data.frame( P=P_seq , cid=1 ) )
+lmu <- apply( lambda , 2 , mean )
+lci <- apply( lambda , 2 , PI )
+lines( pop_seq , lmu , lty=2 , lwd=1.5 )
+shade( lci , pop_seq , xpd=TRUE )
+
+lambda <- link( m11.10 , data=data.frame( P=P_seq , cid=2 ) )
+lmu <- apply( lambda , 2 , mean )
+lci <- apply( lambda , 2 , PI )
+lines( pop_seq , lmu , lty=1 , lwd=1.5 )
+shade( lci , pop_seq , xpd=TRUE )
+
+#Poisson model
+#R code 11.49
+dat2 <- list( T=d$total_tools, P=d$population, cid=d$contact_id )
+m11.11 <- ulam(
+  alist(
+    T ~ dpois( lambda ),
+    lambda <- exp(a[cid])*P^b[cid]/g,
+    a[cid] ~ dnorm(1,1),
+    b[cid] ~ dexp(1),
+    g ~ dexp(1)
+  ), data=dat2 , chains=4 , log_lik=TRUE )
+
+#Plot predictions of Poisson model
+k <- PSIS(m11.11, pointwise = T)$k
+plot( d$population , d$total_tools , xlab="population" , ylab="total tools" ,
+      col=rangi2 , pch=ifelse( dat$cid==1 , 1 , 16 ) , lwd=2 ,
+      ylim=c(0,75) , cex=1+normalize(k) )
+ns <- 100
+P_seq <- seq( from=-5 , to=3 , length.out=ns )
+# 1.53 is sd of log(population)
+# 9 is mean of log(population)
+pop_seq <- exp( P_seq*1.53 + 9 )
+
+#Contact
+lambda <- link( m11.11, data=data.frame( P=pop_seq, cid =1  ) )
+lmu <- apply( lambda , 2 , mean )
+lci <- apply( lambda , 2 , PI(na.rm = T))
+lines( pop_seq , lmu , lty=2 , lwd=1.5 )
+shade( lci , pop_seq , xpd=TRUE )
+
+lambda <- link( m11.11 , data=data.frame( P=pop_seq , cid=2 ) )
+lmu <- apply( lambda , 2 , mean )
+lci <- apply( lambda , 2 , PI )
+lines( pop_seq , lmu , lty=1 , lwd=1.5 )
+shade( lci , pop_seq , xpd=TRUE )
+
+
+#Exposure and onset Monastry example
+#R code 11.50
+num_days <- 30
+y <- rpois( num_days , 1.5 )
+
+
+#Another monastry with weekly data collection
+# If lamda is .5 manuscripts a day
+#R code 11.51
+num_weeks <- 4
+y_new <- rpois( num_weeks , 0.5*7 )
+
+
+#Combine them
+#Data
+#R code 11.52
+y_all <- c( y , y_new )
+exposure <- c( rep(1,30) , rep(7,4) )
+monastery <- c( rep(0,30) , rep(1,4) )
+d <- data.frame( y=y_all , days=exposure , monastery=monastery )
+
+#To fit the model, with different sampling rates the estimated rate of
+# manusxcripts production of each monastry the log of each exposure is computed
+#Then the log days is included in the model
+
+#R code 11.53
+# compute the offset
+d$log_days <- log( d$days )
+
+# fit the model
+m11.12 <- quap(
+  alist(
+    y ~ dpois( lambda ),
+    log(lambda) <- log_days + a + b*monastery,
+    a ~ dnorm( 0 , 1 ),
+    b ~ dnorm( 0 , 1 )
+  ), data=d )
+
+#To compute the posterior of lambda from each monastery and use
+# the linear model (exponent).
+#R code 11.54
+post <- extract.samples( m11.12 )
+lambda_old <- exp( post$a )
+lambda_new <- exp( post$a + post$b )
+precis( data.frame( lambda_old , lambda_new ) )
+
+
+###### Multinomial and categorical models
+
+#The natural link of multinomial distributions is the
+#multinomial logit aka softmax
+
+
+
+#Simulation of career choice
+#R code 11.55
+# simulate career choices among 500 individuals
+N <- 500# number of individuals
+income <- c(1,2,5)# expected income of each career
+score <- 0.5*income # scores for each career, based on income
+# next line converts scores to probabilities
+p <- softmax(score[1],score[2],score[3])
+
+# now simulate choice
+# outcome career holds event type values, not counts
+career <- rep(NA,N) # empty vector of choices for each individual
+# sample chosen career for each individual
+set.seed(34302)
+for ( i in 1:N ) career[i] <- sample( 1:3 , size=1 , prob=p )
+
+
+#R code 11.56
+code_m11.13 <- "
+data{
+  int N; // number of individuals
+  int K; // number of possible careers
+  int career[N]; // outcome
+  vector[K] career_income;
+}
+parameters{
+  vector[K-1] a; // intercepts
+  real<lower=0> b; // association of income with choice
+ 
+}
+model{
+  vector[K] p;
+  vector[K] s;
+  a ~ normal( 0 , 1 );
+  b ~ normal( 0 , 0.5 );
+  s[1] = a[1] + b*career_income[1];
+  s[2] = a[2] + b*career_income[2];
+  s[3] = 0; // pivot
+  p = softmax( s );
+  career ~ categorical( p );
+}
+"
+
+#R code 11.57
+dat_list <- list( N=N , K=3 , career=career , career_income=income )
+m11.13 <- stan( model_code=code_m11.13 , data=dat_list , chains=4 )
+precis( m11.13 , 2 )
+
+
+
+#Doubling the income of category 2
+#R code 11.58
+post <- extract.samples( m11.13 )
+
+# set up logit scores
+s1 <- with( post , a[,1] + b*income[1] )
+s2_orig <- with( post , a[,2] + b*income[2] )
+s2_new <- with( post , a[,2] + b*income[2]*2 )
+
+
+# compute probabilities for original and counterfactual
+p_orig <- sapply( 1:length(post$b) , function(i)
+  softmax( c(s1[i],s2_orig[i],0) ) )
+p_new <- sapply( 1:length(post$b) , function(i)
+  softmax( c(s1[i],s2_new[i],0) ) )
+
+# summarize
+p_diff <- p_new[2,] - p_orig[2,]
+precis( p_diff )
